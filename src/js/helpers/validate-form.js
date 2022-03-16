270da9f6cccfceb96b5c05c16bed059a121b3567
Federico Arzuaga <federico.arzuaga@wundermanthompson.com>
2022-03-16 17:59:01 -0300

    Fixing some issues in the validate funciton

--- src/js/helpers/validate-form.js
+++ src/js/helpers/validate-form.js
@@ -7,14 +7,16 @@ export function validateForm( e ) {
 
     let validate;
 
-    if ( e.target.name.value <= 3 ) {
+    if ( e.target.name.value.length < 3 ) {
+        e.target.name.value = "";
         e.target.name.classList.add('theme-error');
         e.target.name.placeholder = "This field must have at least 3 characters";
         validate = false;
         return validate;
     } 
     
-    if ( e.target.description.value <= 3 ) {
+    if ( e.target.description.value.length < 3 ) {
+        e.target.description.value = "";
         e.target.description.classList.add('theme-error');
         e.target.description.placeholder = "This field must have at least 3 characters";
         validate = false;
